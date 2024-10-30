105f0baaf3cb294605f1c416815686fb00ce5326
MMR <moj2raj2@gmail.com>
2024-10-30 15:38:32 +0330

    init

--- config/reverb.php
+++ config/reverb.php
@@ -76,7 +76,7 @@ return [
                 'app_id' => env('REVERB_APP_ID'),
                 'options' => [
                     'host' => env('REVERB_HOST'),
-                    'port' => 1126,
+                    'port' => env('REVERB_PORT', 443),
                     'scheme' => env('REVERB_SCHEME', 'https'),
                     'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
                 ],
