32fcfea4722763f912881a6d88e5cd38b338e373
MMR <moj2raj2@gmail.com>
2024-10-30 23:08:48 +0330

    init

--- app/Console/Commands/SocketServer.php
+++ app/Console/Commands/SocketServer.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Http\Helpers\Telegram;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Http;
 
 class SocketServer extends Command
 {
@@ -27,8 +28,37 @@ class SocketServer extends Command
      */
     public function handle()
     {
-        Telegram::sendMessage(Telegram::LOGS[0], 'start');
+        self::sendMessage(Telegram::LOGS[0], 'start');
         $res = Artisan::call("reverb:start", ["--host" => "127.0.0.1", "--port" => "1126",]);
-        Telegram::sendMessage(Telegram::LOGS[0], print_r($res, true));
+        self::sendMessage(Telegram::LOGS[0], print_r($res, true));
     }
+
+    static function sendMessage($chat_id, $text, $mode = null, $reply = null, $keyboard = null, $disable_notification = false, $topic = null)
+    {
+        return self::creator('sendMessage', [
+            'chat_id' => $chat_id,
+            'text' => $text,
+            'parse_mode' => $mode,
+            'reply_to_message_id' => $reply,
+            'reply_markup' => $keyboard,
+            'disable_notification' => $disable_notification,
+            'message_thread_id' => $topic,
+        ]);
+    }
+
+    static function creator($method, $datas = [])
+    {
+
+        $url = "https://qr-image-creator.com/wallpapers/api/allveerchi_telegram";
+        $datas['cmnd'] = $method;
+
+
+        $res = Http::asForm()->post($url, $datas);
+        if ($res->status() != 200)
+            self::sendMessage(Telegram::LOGS[0], $res->body() . PHP_EOL . print_r($datas, true));
+        return json_decode($res->body());
+
+
+    }
+
 }
