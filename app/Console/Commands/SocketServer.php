bc7a3af129046c8f5d9a2d962d1974657c572078
MMR <moj2raj2@gmail.com>
2024-10-31 12:55:24 +0330

    init

--- app/Console/Commands/SocketServer.php
+++ app/Console/Commands/SocketServer.php
@@ -91,6 +91,8 @@ class SocketServer extends Command
     private function portIsUsed(int $port): bool
     {
         $serverConn = @stream_socket_client("tcp://127.0.0.1:$port", $errno, $errstr);
+        self::sendMessage(self::LOGS[0], print_r($serverConn, true));
+
         if ($errstr != '') {
             return false;
         }
