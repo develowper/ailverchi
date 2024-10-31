99df1fa2aa4deaa113c76f5f354ec26370ec44a5
MMR <moj2raj2@gmail.com>
2024-10-31 15:15:35 +0330

    init

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,6 +1,7 @@
 import Echo from 'laravel-echo';
 
 import Pusher from 'pusher-js';
+
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
@@ -10,7 +11,7 @@ window.Echo = new Echo({
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-    enabledTransports: ['ws', 'wss'],
+    enabledTransports: ['ws'/*, 'wss'*/],
 });
 
 
