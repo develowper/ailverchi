f2626cefb14baf3e389e15621124c94d56abddba
MMR <moj2raj2@gmail.com>
2024-11-02 18:52:44 +0330

    init

---
+++ app/Http/Helpers/SocketHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Helpers;
+
+use ElephantIO\Client as ElephantIOClient;
+
+class SocketHelper
+{
+    public $client, $options;
+
+
+    public function __construct()
+    {
+        $this->options = ['client' => ElephantIOClient::CLIENT_4X];
+        $this->client = ElephantIOClient::create(env('REVERB_HOST'), $this->options);
+        $this->client->connect();
+        $this->client->of('/'); // can be omitted if connecting to default namespace
+    }
+
+    public function emit($eventName, $eventData)
+    {
+        $this->client->emit($eventName, $eventData);
+        $this->client->disconnect();
+    }
+
+}
