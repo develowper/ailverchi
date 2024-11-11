492a6aebb42637d8fa54c61d7cb911c36cace3ea
MMR <moj2raj2@gmail.com>
2024-11-11 16:13:42 +0330

    init

--- socket/pages/login.vue
+++ socket/pages/login.vue
@@ -5,12 +5,11 @@
 <!--</router>-->
 
 <script lang="ts" setup>
-import {storeToRefs} from 'pinia'; // import storeToRefs helper hook from pinia
 import {useAuthStore} from '~/store/auth'; // import the auth store we just created
 
 const {authenticateUser} = useAuthStore(); // use authenticateUser action from  auth store
 
-const {authenticated} = storeToRefs(useAuthStore()); // make authenticated state reactive with storeToRefs
+const {authenticated} = ref(useAuthStore()); // make authenticated state reactive with storeToRefs
 
 const user = ref({
     username: 'kminchelle',
