61513860e43930897e09babfe1bb3bcc2ca6b2bf
MMR <moj2raj2@gmail.com>
2024-11-12 23:56:42 +0330

    init

--- adonis/vite.config.ts
+++ adonis/vite.config.ts
@@ -1,16 +1,26 @@
-import { defineConfig } from 'vite'
-import { getDirname } from '@adonisjs/core/helpers'
+import {defineConfig} from 'vite'
+import {getDirname} from '@adonisjs/core/helpers'
 import inertia from '@adonisjs/inertia/client'
 import vue from '@vitejs/plugin-vue'
 import adonisjs from '@adonisjs/vite/client'
 
 export default defineConfig({
-  plugins: [inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.ts' } }), vue(), adonisjs({ entrypoints: ['inertia/app/app.ts'], reload: ['resources/views/**/*.edge'] })],
+  plugins: [inertia({
+    ssr: {
+      enabled: true,
+      entrypoint: 'inertia/app/ssr.ts'
+    }
+  }), vue(), adonisjs({entrypoints: ['inertia/app/app.ts'], reload: ['resources/views/**/*.edge']})],
 
   /**
    * Define aliases for importing modules from
    * your frontend code
    */
+  server: {
+    hmr: {
+      clientPort: 9204
+    }
+  },
   resolve: {
     alias: {
       '~/': `${getDirname(import.meta.url)}/inertia/`,
